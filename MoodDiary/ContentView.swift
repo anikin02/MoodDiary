cae21a19b6795e1ef1cea831ed151f20b58770c4
Danil Anikin <mart4r.clearlist@gmail.com>
2025-04-12 12:37:23 +1000

    Initial Commit

---
+++ MoodDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodDiary
+//
+//  Created by Данил Аникин on 12.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodDiary/MoodDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodDiaryApp.swift
+//  MoodDiary
+//
+//  Created by Данил Аникин on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
